d8aab5cb94896c42df363bc08ddf3fd7cb38fa89
ksirbj <jakebrisk@gmail.com>
2016-08-18 21:31:46 -0400

    Convo tracker only

--- tools/clock-in-out/background/background.js
+++ tools/clock-in-out/background/background.js
@@ -1,6 +1,6 @@
 // global variables 
-var trackSmartResponse = null;
-var slackResponse = null;
+//var trackSmartResponse = null;
+//var slackResponse = null;
 var userData = {
 	working: false,
 	totalHours: 0,
@@ -30,39 +30,39 @@ chrome.storage.sync.get({'working': false, 'totalHours': 0, 'totalClosedConvos':
 
 // open up the tabs when the user clocks in
 function clockIn(){
-	chrome.tabs.create({active: false, url: "https://timeclock.tracksmart.com/app/time"}, 
-		function(tab){
-			chrome.tabs.executeScript(tab.id,{
-				file: "deploy/login.js",
-				runAt: "document_end"
-			}, function(){
-				chrome.tabs.sendMessage(tab.id, {message: "clock-in"});
-				if(chrome.runtime.lastError){
-					console.log(chrome.runtime.lastError);
-				}
-			});
-		}
-	);
+	// chrome.tabs.create({active: false, url: "https://timeclock.tracksmart.com/app/time"}, 
+	// 	function(tab){
+	// 		chrome.tabs.executeScript(tab.id,{
+	// 			file: "deploy/login.js",
+	// 			runAt: "document_end"
+	// 		}, function(){
+	// 			chrome.tabs.sendMessage(tab.id, {message: "clock-in"});
+	// 			if(chrome.runtime.lastError){
+	// 				console.log(chrome.runtime.lastError);
+	// 			}
+	// 		});
+	// 	}
+	// );
 
-	chrome.tabs.create({active: false, url: "https://codecademy.slack.com/messages/proadvisors/details/"}, 
-		function(tab){
-			chrome.tabs.executeScript(tab.id, {file: "libs/moment.min.js"}, function(){
-				chrome.tabs.executeScript(tab.id, { file: "libs/jquery-2.1.4.js"}, function(){
-					chrome.tabs.executeScript(tab.id, { file: "deploy/slack.js", runAt: "document_end" }, function(){
-						chrome.tabs.sendMessage(tab.id, {message: "clock-in"});
-						if(chrome.runtime.lastError){
-							console.log(chrome.runtime.lastError);
-						}
-					});
-				})
-			});
-		}
-	);
-	chrome.tabs.create({url: "https://app.intercom.io/a/apps/wft4jxth/inbox/unassigned/conversations"}, function(){
-		if(chrome.runtime.lastError){
-			console.log(chrome.runtime.lastError);
-		}
-	});
+	// chrome.tabs.create({active: false, url: "https://codecademy.slack.com/messages/proadvisors/details/"}, 
+	// 	function(tab){
+	// 		chrome.tabs.executeScript(tab.id, {file: "libs/moment.min.js"}, function(){
+	// 			chrome.tabs.executeScript(tab.id, { file: "libs/jquery-2.1.4.js"}, function(){
+	// 				chrome.tabs.executeScript(tab.id, { file: "deploy/slack.js", runAt: "document_end" }, function(){
+	// 					chrome.tabs.sendMessage(tab.id, {message: "clock-in"});
+	// 					if(chrome.runtime.lastError){
+	// 						console.log(chrome.runtime.lastError);
+	// 					}
+	// 				});
+	// 			})
+	// 		});
+	// 	}
+	// );
+	// chrome.tabs.create({url: "https://app.intercom.io/a/apps/wft4jxth/inbox/unassigned/conversations"}, function(){
+	// 	if(chrome.runtime.lastError){
+	// 		console.log(chrome.runtime.lastError);
+	// 	}
+	// });
 
 	// set the working variable to true 
 	userData.working = true;
@@ -82,37 +82,37 @@ function clockOut(){
 	// no longer working
 	userData.working = false;
 	// clock out on slack
-	chrome.tabs.create({active: false, url: "https://codecademy.slack.com/messages/proadvisors/details/"}, 
-		function(tab){
-			chrome.tabs.executeScript(tab.id, {file: "libs/moment.min.js"}, function(){
-				chrome.tabs.executeScript(tab.id, { file: "libs/jquery-2.1.4.js"}, function(){
-					chrome.tabs.executeScript(tab.id, { file: "deploy/slack.js", runAt: "document_end" }, function(){
-						chrome.tabs.sendMessage(tab.id, {message: "clock-out"});
-						if(chrome.runtime.lastError){
-							console.log(chrome.runtime.lastError);
-						}
-					});
-				})
-			});
-		}
-	);
+	// chrome.tabs.create({active: false, url: "https://codecademy.slack.com/messages/proadvisors/details/"}, 
+	// 	function(tab){
+	// 		chrome.tabs.executeScript(tab.id, {file: "libs/moment.min.js"}, function(){
+	// 			chrome.tabs.executeScript(tab.id, { file: "libs/jquery-2.1.4.js"}, function(){
+	// 				chrome.tabs.executeScript(tab.id, { file: "deploy/slack.js", runAt: "document_end" }, function(){
+	// 					chrome.tabs.sendMessage(tab.id, {message: "clock-out"});
+	// 					if(chrome.runtime.lastError){
+	// 						console.log(chrome.runtime.lastError);
+	// 					}
+	// 				});
+	// 			})
+	// 		});
+	// 	}
+	// );
 
-	// clock out on tracksmart
-	chrome.tabs.create({active: false, url: "https://timeclock.tracksmart.com/app/time"}, 
-		function(tab){
-			chrome.tabs.executeScript(tab.id,{
-				file: "deploy/login.js",
-				runAt: "document_end"
-			}, function(){
-				chrome.tabs.sendMessage(tab.id, {message: "clock-out"});
-				if(chrome.runtime.lastError){
-					console.log(chrome.runtime.lastError);
-				}
-			});
-		}
-	);
-	// open the post shift report tab
-	chrome.tabs.create({active: false, url: "https://docs.google.com/forms/d/e/1FAIpQLSciPORNy-a1iO-75rLK_suwJaBfWGzA-GlQPVVLAvRNQ_DW5w/viewform"});
+	// // clock out on tracksmart
+	// chrome.tabs.create({active: false, url: "https://timeclock.tracksmart.com/app/time"}, 
+	// 	function(tab){
+	// 		chrome.tabs.executeScript(tab.id,{
+	// 			file: "deploy/login.js",
+	// 			runAt: "document_end"
+	// 		}, function(){
+	// 			chrome.tabs.sendMessage(tab.id, {message: "clock-out"});
+	// 			if(chrome.runtime.lastError){
+	// 				console.log(chrome.runtime.lastError);
+	// 			}
+	// 		});
+	// 	}
+	// );
+	// // open the post shift report tab
+	// chrome.tabs.create({active: false, url: "https://docs.google.com/forms/d/e/1FAIpQLSciPORNy-a1iO-75rLK_suwJaBfWGzA-GlQPVVLAvRNQ_DW5w/viewform"});
 	// update the total convos (totalHours is updated by popup.js)
 	userData.totalClosedConvos += userData.closedThisShift;
 	// store data for later use
